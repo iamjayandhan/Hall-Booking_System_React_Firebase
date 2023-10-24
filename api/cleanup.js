624a7db6902d1f79e64e56544c6e47fc660f2ae8
Jayandhan <jayandhanrajendran03@gmail.com>
2023-10-24 12:36:02 +0530

    cronjobs

--- api/cleanup.js
+++ api/cleanup.js
@@ -1,31 +1,33 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp();
-
-// Define a Firebase scheduled function
-exports.cleanupExpiredBookings = functions.pubsub.schedule('every 2 minutes').timeZone('Asia/Kolkata').onRun((context) => {
-  const now = new Date();
-
-  // Reference to the "bookings" collection in Firestore
-  const bookingsRef = admin.firestore().collection('bookings');
-
-  // Query bookings that have ended
-  const expiredBookingsQuery = bookingsRef.where('endTime', '<', now);
-
-  // Delete expired bookings
-  return expiredBookingsQuery.get()
-    .then((querySnapshot) => {
-      const batch = admin.firestore().batch();
-      querySnapshot.forEach((doc) => {
-        batch.delete(doc.ref);
-      });
-      return batch.commit();
-    })
-    .then(() => {
-      console.log('Expired bookings cleaned up successfully.');
-      return null;
-    })
-    .catch((error) => {
-      console.error('Error cleaning up expired bookings:', error);
-    });
-});
+export default async function handler(req, res) {
+    // Initialize Firebase Admin SDK
+    const admin = require("firebase-admin");
+    admin.initializeApp();
+  
+    const now = new Date();
+  
+    // Reference to the "bookings" collection in Firestore
+    const db = admin.firestore();
+    const bookingsRef = db.collection("bookings");
+  
+    // Query bookings that have ended
+    const expiredBookingsQuery = bookingsRef.where("endTime", "<", now);
+  
+    try {
+      const querySnapshot = await expiredBookingsQuery.get();
+  
+      if (!querySnapshot.empty) {
+        const batch = db.batch();
+        querySnapshot.docs.forEach((doc) => {
+          batch.delete(doc.ref);
+        });
+        await batch.commit();
+        console.log("Expired bookings cleaned up successfully.");
+      }
+  
+      res.status(200).end("Cleanup completed.");
+    } catch (error) {
+      console.error("Error cleaning up expired bookings:", error);
+      res.status(500).end("Cleanup failed.");
+    }
+  }
+  
