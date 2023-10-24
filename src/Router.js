5dc38632e0c9f0e57fee64604eefe4c94881f503
Jayandhan <jayandhanrajendran03@gmail.com>
2023-10-24 14:23:52 +0530

    clean

--- src/Router.js
+++ src/Router.js
@@ -4,7 +4,6 @@ import { Route, Routes } from 'react-router-dom';
 import Registration from './Registration';
 import Login from './Login';
 import MainPage from './MainPage';
-import Availability from './Availability'
 
 const AppRouter = () => {
   return (
@@ -13,7 +12,6 @@ const AppRouter = () => {
       <Route path="/registration" element={<Registration />} />
       <Route path="/login" element={<Login />} />
       <Route path="/MainPage" element={<MainPage />} />
-      <Route path="/Availability" element={<Availability/>} />
     </Routes>
   );
 };
