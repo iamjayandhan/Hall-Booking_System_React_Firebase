8faa29ea3667bfd7f7f7796df43d601b74c7ff38
Jayandhan <jayandhanrajendran03@gmail.com>
2023-10-25 14:29:29 +0530

    vercel run

--- src/cleanup.js
+++ src/cleanup.js
@@ -1,20 +1,17 @@
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+
+// Initialize Firebase Admin
+admin.initializeApp(functions.config().firebase);
 
 const cleanup = async () => {
-  const serviceAccount = require("./key.json");
-
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://hall-allocation-c720d.firebaseio.com",
-  });
-
   const now = new Date();
 
   // Reference to the "bookings" collection in Firestore
   const db = admin.firestore();
-  const bookingsRef = db.collection("bookings");
+  const bookingsRef = db.collection('bookings');
 
-  console.log("Cleanup function started at", now.toISOString());
+  console.log('Cleanup function started at', now.toISOString());
 
   try {
     // Create a query for all bookings
@@ -29,26 +26,35 @@ const cleanup = async () => {
     let deletedCount = 0;
 
     bookingsSnapshot.forEach((doc) => {
-      const bookingEndTime = new Date(doc.data().date + "T" + doc.data().endTime);
+      const bookingEndTime = new Date(doc.data().date + 'T' + doc.data().endTime);
       if (bookingEndTime <= now) {
         batch.delete(doc.ref);
-        console.log("Deleted expired booking:", doc.id);
+        console.log('Deleted expired booking:', doc.id);
         deletedCount++;
       } else {
-        console.log("Skipped booking (not expired):", doc.id);
+        console.log('Skipped booking (not expired):', doc.id);
         skippedCount++;
       }
     });
 
     if (deletedCount > 0) {
       await batch.commit();
-      console.log("Expired bookings cleaned up successfully.");
+      console.log('Expired bookings cleaned up successfully.');
     } else if (skippedCount > 0) {
-      console.log("No expired bookings found.");
+      console.log('No expired bookings found.');
     }
   } catch (error) {
-    console.error("Error cleaning up expired bookings:", error);
+    console.error('Error cleaning up expired bookings:', error);
   }
 };
 
-module.exports = cleanup; // Export the cleanup function
+module.exports = functions.https.onRequest((req, res) => {
+  cleanup()
+    .then(() => {
+      res.status(200).send('Cleanup completed successfully');
+    })
+    .catch((error) => {
+      console.error('Cleanup failed:', error);
+      res.status(500).send('Cleanup failed');
+    });
+});
