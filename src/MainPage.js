ceb1c75b833b379db14465b14995494bdd3c3c78
Jayandhan <jayandhanrajendran03@gmail.com>
2023-10-26 14:20:13 +0530

    DONE

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,4 +1,4 @@
-  import React, { useState,useEffect } from 'react';
+  import React, { useState } from 'react';
   import './MainPage.css';
   import { db } from './firebase';
   import { addDoc,collection, getDocs, query, where } from 'firebase/firestore';
